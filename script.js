2a4d132b18e87d4961c1a10501479b076814e297
Orion <gn9081ca@go.minneapolis.edu>
2021-03-22 17:36:24 -0500

    Header in HTML and User Object in Script

---
+++ script.js
@@ -0,0 +1,15 @@
+//api calls for jokes
+let joke_apis = [
+    {"name":"JokeAPIDev","url":'https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,racist,sexist,explicit'},
+    {"name":"JokesOne","url":'https://api.jokes.one/jod?category=animal'},
+
+]
+
+let current_user = {
+    "name":"",
+    "Jokes":{
+        
+    }
+}
+
+
