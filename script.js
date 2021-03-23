998125e0b76aa210c94bbf08044a2baa0c68ca7d
Orion <gn9081ca@go.minneapolis.edu>
2021-03-23 14:22:28 -0500

    I know why the filljokelist while loop is infinite

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 //api calls for jokes
-let joke_apis = [
-    {"name":"JokeAPI","url":'https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,racist,explicit&type=single', "called": false}
-]
-
+const joke_api_url = 'https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,racist,explicit&type=single'
+const jokeofhteday_api_url = 'https://api.jokes.one/jod?category='
 //Current user object broken down because there were errors with limiting the api calls related to this object
 let current_user_name =""
 let current_user_jokes = []
@@ -30,62 +28,37 @@ function set_user_name(new_name){
 //Name submission event listener
 nameSubmitButton.addEventListener('click', function() {set_user_name(nameText.value)})
 
-// This function causes the browser to hang
+//Joke API Fetch Function
 function getJoke() {
-    {        
-        fetch(joke_apis[0].url).then( response => response.json()).then( (result) =>{
+    fetch(joke_api_url)
+        .then( response => response.json()).then( (result) =>{
+            //console.log(result)
             let jokeString = result.joke
-            let apiName = api.name
+            let apiName = 'JokeAPI'
             let jokeObj = {"JokeString":jokeString,"Source":apiName,"Rating": 0}
             current_user_jokes.push(jokeObj)
-        }
-        ).catch( error => {
-            alert('Something went wrong')  // user friendly - most users can't do anything about a stack trace
-            console.log(error)  // for the developer to debug the app
-            window.stop()
-        })
-
+    }
+    )
+    .catch( error => {
+        alert('Something went wrong. JokeAPI.dev is unavailable.')  // user friendly - most users can't do anything about a stack trace
+        console.log(error)  // for the developer to debug the app
+        window.stop()
     })
-
 }
-
-
-//Updated getJoke function to address the hanging issues
-function get_joke_API(apiURL) {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-	 if (this.readyState == 4 && this.status == 200) {
-         let response = this.responseText.json()
-	     // Access the result here
-            let jokeString = result.joke
-            let apiName = api.name
-            let jokeObj = {"JokeString":jokeString,"Source":apiName,"Rating": 0}
-            current_user_jokes.push(jokeObj)
-         //console.log(this.responseText)
-	 }
-    };
-    xhttp.open("GET", apiURL, true);
-    xhttp.setRequestHeader("Content-type", "application/json");
-    xhttp.setRequestHeader("X-JokesOne-Api-Secret", "YOUR API HERE");
-    xhttp.send();
-}
-
-
 
 //From https://jokes.one/api/joke/#js
 function get_joke_of_the_day(category) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
 	 if (this.readyState == 4 && this.status == 200) {
-         let response = this.response.json()
-         console.log(typeof(response))
-         console.log(`https://api.jokes.one/jod?category=${category}`)
-         console.log(response.success)
-	     // Access the result here
-         let jokeString = response.contents.jokes[0].joke
-         let apiName = response.contents.jokes[0].description
-         let jokeObj = {"JokeString":jokeString,"Source":apiName,"Rating": 0}
-        //  current_user_jokes.push(jokeObj)
+        let response = JSON.parse(this.response)
+        //console.log(`https://api.jokes.one/jod?category=${category}`)
+        //console.log(response.contents.jokes[0].description)
+	    // Access the result here
+        let jokeString = response.contents.jokes[0].joke.text
+        let apiName = response.contents.jokes[0].description
+        let jokeObj = {"JokeString":jokeString,"Source":apiName,"Rating": 0}
+        current_user_jokes.push(jokeObj)
          
 	 }
     };
@@ -95,13 +68,12 @@ function get_joke_of_the_day(category) {
     xhttp.send();
 }
 
+//TODO create function that removes duplicates from list.
 
+//TODO create function that sorts list by rating. This will be refreshed each time the user rates a joke.
 
-
-
-
-
-
+//JOD_categories.forEach((category) => {get_joke_of_the_day(category)})
+console.log(current_user_jokes)
 
 function wait(delay) {
     const beginning = Date.now()
@@ -114,20 +86,37 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-//console.log(typeof(current_user_jokes))
+const waitPromise = new Promise((resolve, reject) => {
 
-function createJokeList() {
-    //let i = 0
-    JOD_categories.forEach((category) => {get_joke_of_the_day(category)})
-    while (current_user_jokes.length < 10) {
-        joke_apis.forEach((api) => {get_joke_API(api)})
-        //debugging
-        let jokeObj = {"JokeString":"jokeString","Source":"apiName","Rating": 0}
-        current_user_jokes.push(jokeObj)
+})
 
-    }
-    console.log('Joke list completed')
-    console.log(current_user_jokes)
+function fillJokeList() {
+
+    do {getJoke()} while (current_user_jokes.length < 10)
+    //  {
+    //     console.log(current_user_jokes.length)
+
+    //     getJoke().then(console.log(current_user_jokes.length))
+
+    //     //debugging joke list filler
+    //     // let jokeObj = {"JokeString":"jokeString","Source":"apiName","Rating": 0}
+    //     // current_user_jokes.push(jokeObj)
+
+    // }
+    // console.log('Joke list completed')
+    // console.log(current_user_jokes)
 }
 
-createJokeList()
+function bring_out_the_kraken() {
+
+    JOD_categories.forEach((category) => {get_joke_of_the_day(category)})
+
+    //This seems to happen before the above line of code and I don't know why.
+    // fillJokeList()
+    console.log(current_user_jokes.length)
+    
+
+}
+
+bring_out_the_kraken()
+console.log(current_user_jokes)
